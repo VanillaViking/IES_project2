178985100679ad66d09950d9d62a446964cdf35b
Ashwin Rajesh <ashwinr2k2@gmail.com>
2024-05-19 13:36:19 +1000

    feat: rotary button

--- src/main.c
+++ src/main.c
@@ -6,16 +6,43 @@
 #include "usart.h"
 #include "bit.h"
 
-volatile bool rotary_pin_a = 0;
-volatile bool rotary_pin_b = 0;
+#define ROTARY_CLK PD7
+#define ROTARY_DT PD3
+// normally open, shorted to GND on press
+#define ROTARY_BTN PD2
+
+volatile float motor_duty = 0.5;
+//prevents external interrupt from triggering too many increments
 volatile bool debounce = 0;
+volatile bool rotary_button_toggle = 0;
 
 ISR(INT1_vect) {
+  if (debounce) return;
   debounce = 1;
+  _delay_ms(10);
 
-  for (int i = 0; i < 10; i++) {
-    rotary_pin_a = bitRead(PIND, PD2);
-    rotary_pin_b = bitRead(PIND, PD3);
+  if (bitRead(PIND, ROTARY_CLK) == bitRead(PIND, ROTARY_DT)) {
+    // clockwise
+    motor_duty += 0.05;
+  } else {
+    // anti clock
+    motor_duty -= 0.05;
+  }
+
+  if (motor_duty > 0.9) {
+    motor_duty = 0.9;
+  }
+  if (motor_duty < 0) {
+    motor_duty = 0;
+  }
+
+}
+
+// rotary button
+ISR(INT0_vect) {
+  _delay_ms(10);
+  if (bitRead(PIND, ROTARY_BTN) == 0) {
+    rotary_button_toggle = !rotary_button_toggle;
   }
 }
 
@@ -27,19 +54,23 @@ int main(void) {
 
   bitSet(DDRB,PB5);
 
-  bitClear(DDRD,PD2);
-  bitClear(DDRD,PD3);
+  bitClear(DDRD,ROTARY_CLK);
+  bitClear(DDRD,ROTARY_DT);
+  bitClear(DDRD,ROTARY_BTN);
   
-  /* bitSet(EICRA, ISC01); */
-  /* bitClear(EICRA, ISC00); */
-  /* bitSet(EIMSK, INT0); */
+  // enable pullup resistor
+  bitSet(PORTD, ROTARY_BTN);
+  
+  bitSet(EICRA, ISC01);
+  bitClear(EICRA, ISC00);
+  bitSet(EIMSK, INT0);
 
-  bitSet(EICRA, ISC11);
-  bitClear(EICRA, ISC10);
+  bitClear(EICRA, ISC11);
+  bitSet(EICRA, ISC10);
   bitSet(EIMSK, INT1);
 
   sei();
-  
+
   while(1)
   {
     bitSet(PORTB,PB5);
@@ -47,10 +78,11 @@ int main(void) {
     bitClear(PORTB,PB5);
     _delay_ms(100);
     debounce = 0;
-  
-    usart_tx_float(rotary_pin_a, 1, 1);
-    usart_transmit(',');
-    usart_tx_float(rotary_pin_b, 1, 1);
+
+    usart_tx_string(">a:");
+    usart_tx_float(motor_duty, 1, 2);
+    usart_tx_string(">b:");
+    usart_tx_float(rotary_button_toggle, 1, 2);
     usart_transmit('\n');
   }
 }

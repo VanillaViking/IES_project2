75b08284c27ad61114dde72db70ca50569795ad7
ajsadsad <z5208382@ad.unsw.edu.au>
2024-05-24 11:30:52 +1000

    Added Shaman's sxc work

--- src/main.c
+++ src/main.c
@@ -21,7 +21,20 @@
 #define POTENTIOMETER PD7
 #define INPUT PC0
 
-#define BUFFER_SIZE 50
+#define CHAR_BUFFER 25
+#define TOP 2000
+#define CLOCKF 1000 // Frequency of T1 clock
+
+int COMPARE = 0;  // Set if Thermistor value is > Potentiometer value
+uint16_t therm_read, pot_read;
+char status[CHAR_BUFFER];
+char system_uptime[CHAR_BUFFER];
+bool risingcaptured = 0;
+double numOv = 0;
+double ICtimecapt;
+double FanTURNONtime;
+double FanTURNOFFtime;
+bool status_changed = 0;
 
 enum ROTARY_STATE {
   START,
@@ -35,13 +48,41 @@ volatile float motor_duty = 0.7;
 volatile bool rotary_button_toggle = 1;
 volatile int rotary_state = START;
 
-unsigned char data_buffer[BUFFER_SIZE];
-unsigned char *pTx = data_buffer;
-
 void adc_init();
+void t1_init();
 float result_to_tempC(uint16_t result);
+void clock_to_time(double clocktime, char timedisp[]);
 void handle_rotary();
 
+ISR(TIMER1_COMPA_vect){
+    numOv++;
+}
+
+/* ISR for handling AC output changes - Fan state */
+ISR(ANALOG_COMP_vect){
+    status_changed = 1; // Fan status has been changed as ISR has been triggered
+    if(bitRead(ACSR,ACO)){ // rising edge has been detected
+        strcpy(status,"FAN TURNED ON AT - ");
+    }
+    if(!bitRead(ACSR,ACO)){ // falling edge has been detected
+
+        strcpy(status,"FAN TURNED OFF AT - ");
+    }
+    strcat(status,system_uptime); // Combine status and system uptime string for displaying
+}
+
+/* ISR for capturing time stamps of Fan state changes */
+ISR(TIMER1_CAPT_vect){
+    if(COMPARE == 0){ // rising edge has been captured
+        FanTURNONtime = (ICR1 + numOv*TOP)/TOP;
+        bitClear(TCCR1B,ICES1); // Capture on FALLING edge
+    }
+    if(COMPARE == 1){ // falling edge has been captured
+        FanTURNOFFtime = (ICR1 + numOv*TOP)/TOP;
+        bitSet(TCCR1B,ICES1); // Capture on RISING edge
+    }
+    _delay_ms(30);
+}
 // DT INTERRUPT
 ISR(INT1_vect) {
   bool dt = bitRead(PIND, ROTARY_DT);
@@ -111,18 +152,20 @@ ISR(INT0_vect) {
 }
 
 int main(void) {
-  // beginning of pain
-  cli();
-  usart_init(8);
-  adc_init();
-
-  // Set as inputs
+  // I/O ports
   bitClear(DDRD,THERMISTOR);    // AIN0
   bitClear(DDRD,POTENTIOMETER); // AIN1
 
+  bitSet(ACSR,ACIE); // Analog Comparator Interrupt Enable
+
+  char LiveFanOnTime[CHAR_BUFFER];
+  char PrevFanOnTime[CHAR_BUFFER];
+  char FanTurnOnTime[CHAR_BUFFER];
+
   int channelswap = 0;
-  int COMPARE = 0;
-  uint16_t therm_read, pot_read;
+  // Set as inputs
+  bitClear(DDRD,THERMISTOR);    // AIN0
+  bitClear(DDRD,POTENTIOMETER); // AIN1
 
   bitClear(DDRD,ROTARY_CLK);
   bitClear(DDRD,ROTARY_DT);
@@ -159,8 +202,11 @@ int main(void) {
   bitClear(PORTB, M1_NGTV_DIRECTION);
 
   OCR0A = 78;
-
+  usart_init(8); // 103-9600 bps; 8-115200
+  adc_init();
+  t1_init();
   sei();
+
   while(1)
   {
     COMPARE = bitRead(ACSR,ACO); // Determine AC output
@@ -169,9 +215,23 @@ int main(void) {
 
     if(COMPARE > 0 && rotary_button_toggle) {
       OCR0B = (int)(78 * motor_duty);
-    }
-    else {
+      clock_to_time(numOv-FanTURNONtime,LiveFanOnTime);
+      usart_tx_string(">Fan Uptime:");
+      usart_tx_string("Fan ON duration - ");
+      usart_tx_string(LiveFanOnTime);
+      usart_tx_string("|t");
+      usart_transmit('\n');
+    } else {
       OCR0B = 1;
+      clock_to_time(abs(FanTURNOFFtime - FanTURNONtime),PrevFanOnTime);
+      clock_to_time(FanTURNONtime,FanTurnOnTime);
+      usart_tx_string(">Fan Uptime:");
+      usart_tx_string("Fan OFF, was last turned ON at ");
+      usart_tx_string(FanTurnOnTime);
+      usart_tx_string(" for duration - ");
+      usart_tx_string(PrevFanOnTime);
+      usart_tx_string("|t");
+      usart_transmit('\n');
     }
 
     usart_tx_string(">a:");
@@ -236,6 +296,67 @@ void handle_rotary() {
       rotary_button_toggle = !rotary_button_toggle;
     }
   }
+}
+
+/* Initialises Timer1 - used for Input Capture */
+void t1_init()
+{
+    bitSet(ACSR, ACIC);      // Enable the input capture function in TCNT1 to be triggered by AC
+    bitSet(TIMSK1, ICIE1);   // TCNT1 Input Capture Interrupt Enable
+    bitSet(TCCR1B, ICES1);   // Capture on RISING edge
+    bitClear(TCCR1B, WGM13); // Enable TCNT1 as CTC mode
+    bitSet(TCCR1B, ICNC1);   // Input Capture Noise Canceler
+    bitSet(TCCR1B, WGM12);
+    bitClear(TCCR1A, WGM11);
+    bitClear(TCCR1A, WGM10);
+    TCCR1B &= 0xF8; // Set prescaler = 1024
+    TCCR1B |= 2;
+    OCR1A = TOP - 1;
+    bitSet(TIMSK1, OCIE1A); // Timer/Counter1, Output Compare A Match Interrupt Enable
+}
+
+void clock_to_time(double clocktime, char timedisp[])
+{
+    int sechour = 60; // Number of seconds in an hour. Variable for debugging and clock testing purposes.
+    // Math functions for converting T1 clock value into hh:mm:ss.ms
+    // Note: this does not account for days because I do not expect anyone to run this arduino for a whole ass day
+    double hours   = (int)((clocktime / CLOCKF) / sechour) / sechour;
+    double minutes = (int)((clocktime / CLOCKF) / sechour - hours * sechour);
+    double seconds =      ( clocktime / CLOCKF - (minutes + hours * sechour) * sechour);
+
+    // Create strings for printing time value
+    char str_s[6];
+    char str_m[3];
+    char str_h[3];
+
+    // Change double time values to respective strings
+    dtostrf(seconds, 5, 2, str_s);
+    dtostrf(minutes, 2, 0, str_m);
+    dtostrf(hours, 2, 0, str_h);
+
+    // Merge all strings into one time display string (goofy hardcode)
+    int len = 6;
+    if(hours > 0)
+    {
+        len = 0;
+        timedisp[0-len] = str_h[0];
+        timedisp[1-len] = str_h[1];
+        timedisp[2-len] = 'h';
+    }
+    if(minutes > 0)
+    {
+        len = 3;
+        timedisp[3-len] = str_m[0];
+        timedisp[4-len] = str_m[1];
+        timedisp[5-len] = 'm';
+    }
+    timedisp[6-len] = str_s[0];
+    timedisp[7-len] = str_s[1];
+    timedisp[8-len] = str_s[2];
+    timedisp[9-len] = str_s[3];
+    timedisp[10-len] = str_s[4];
+    timedisp[11-len] = 's';
+    timedisp[12-len] = '\0';
 }
 
 float result_to_tempC(uint16_t result)
